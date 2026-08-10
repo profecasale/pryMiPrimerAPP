533661d4cdd38a85641283508c39af0d9b0918eb
Alumno <Alumno@PC174>
2026-08-10 10:58:46 -0300

    Agregar archivos de proyecto.

---
+++ pryMiPrimerAPP/frmPrincipal.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace pryMiPrimerAPP
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAceptar = new Button();
+            SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(56, 42);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(227, 79);
+            btnAceptar.TabIndex = 0;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(356, 164);
+            Controls.Add(btnAceptar);
+            Name = "frmPrincipal";
+            Text = "Mí primero APP";
+            Load += frmPrincipal_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAceptar;
+    }
+}
---
+++ pryMiPrimerAPP/frmPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pryMiPrimerAPP
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ouchie!!!");
+        }
+    }
+}
